f0bccf6598e5e6ad42cfe65e944e6af50027bc9a
shashidharyellenki <yellenkishashidhar@msitprogram.net>
2022-05-06 21:59:01 +0530

    inital commit

---
+++ forms/models.py
@@ -0,0 +1,19 @@
+import email
+from django.db import models
+from datetime import datetime, date
+
+from numpy import true_divide
+# Create your models here.
+class Register(models.Model):
+    FirstName = models.CharField(max_length=100, blank=False)
+    LastName = models.CharField(max_length=100, blank=False)
+    email = models.EmailField(max_length=100, blank=False)
+    phone = models.IntegerField(blank=False)
+    password = models.CharField(max_length=200, blank=False)
+    password2 = models.CharField(max_length=200, blank=False)
+    Birthday_date = models.DateField(auto_now=False)
+    username = models.CharField(max_length=100, blank=True, unique=True)
+
+
+    def __str__(self):
+        return self.username
